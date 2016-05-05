9e2eeafc8fcdf7ab9b4c1255fe6c3c6b48ca2639
yshi13 <yi-shi-1@uiowa.edu>
2016-05-04 23:39:44 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $("#start").datepicker();
-    $("#end").datepicker();
-    $("button").click(function() {
-    	var selected = $("#dropdown option:selected").text();
-        var start = $("#start").val();
-        var end = $("#end").val();
-    });
-});
