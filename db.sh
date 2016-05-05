0b5a17857ee6efe4064488af1e15dd704440ad99
yshi13 <yi-shi-1@uiowa.edu>
2016-05-04 23:44:08 -0500

    Delete db.sh

--- db.sh
+++
@@ -1,4 +0,0 @@
-# Change DBPASSWORD, DBUSER, DBHOST and DBNAME to match the values
-# your mysql_db_info file on the webdev server.
-
-mysql --password='WSFMxVGKdxDC' --user='yshi13' --host='dbdev.cs.uiowa.edu' 'db_yshi13'
